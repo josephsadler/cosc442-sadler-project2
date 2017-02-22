68d64a4d5633448ddeead90c9b3d9ec72ed1edb4
Joseph <jsadle5@students.towson.edu>
2017-02-22 12:30:06 -0500

    Task 3 - Rectangle
    Bug was located in Point class and corrected

---
+++ Rectangle/src/edu/towson/cis/cosc442/project2/rectangle/Point.java
@@ -0,0 +1,21 @@
+package edu.towson.cis.cosc442.project2.rectangle;
+
+/**
+ * The Point Class.
+ */
+public class Point {
+	
+	/** x and y coordinates. */
+	public Double x, y;
+	
+	/**
+	 * Instantiates a new point.
+	 *
+	 * @param xVal the x
+	 * @param yVal the y
+	 */
+	Point(Double xVal, Double yVal) {
+		x = xVal; //x = y was corrected
+		y = yVal;
+	}
+}
